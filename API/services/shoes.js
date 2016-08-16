d1414884e05ae4f3958169063393d659d9a3e84c
hpphat92 <hpphat1992@gmail.com>
2016-08-17 00:10:13 +0700

    fix api search shoe

--- API/services/shoes.js
+++ API/services/shoes.js
@@ -4,12 +4,11 @@ module.exports = function(shoes){
     searchShoes: function(query){
       var list = _.filter(shoes, function(shoe){
         return _.find(['shoeName','seller','description'], function(key){
-          return shoe[key].toUpperCase().indexOf(query.keyword.toUpperCase())>=0;
+          return shoe[key].toUpperCase().indexOf((query.keyword || '').toUpperCase())>=0;
         });
       });
       return {
         totalCount: list.length,
-        query: query,
         list: list.splice(query.skip || 0, query.take || 10)
       };
     }
