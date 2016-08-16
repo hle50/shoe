4c72dc51fc6b18a2d940070b941f353ef5988e25
hoale <ledanghoa1984@outlook.com>
2016-08-16 22:42:13 +0700

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xxxxApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the xxxxApp
+ */
+angular.module('xxxxApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
