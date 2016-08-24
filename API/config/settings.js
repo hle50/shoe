59e9c2f9722fcbe348f71e8d467c9df05e3df13d
hoale <ledanghoa1984@outlook.com>
2016-08-24 22:10:56 +0700

    change connection string

--- API/config/settings.js
+++ API/config/settings.js
@@ -2,5 +2,5 @@ module.exports = {
   appKey: '2bf894jas839327djg48',
   appSecret: '8dngk594017f85ji69506895s',
   apiKey: 'a78j4dr85895db90124894gb6969auy',
-  mongoConnectString: 'mongodb://localhost/testShoes1'
+  mongoConnectString: 'mongodb://test:test@ds013966.mlab.com:13966/shoes'
 };
